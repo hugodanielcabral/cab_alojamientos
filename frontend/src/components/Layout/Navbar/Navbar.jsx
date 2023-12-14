ff303a1ed542b2a7f0e52762a17317a1372b9081
Daniel Cabral <danicabral15@outlook.com>
2023-12-14 15:17:13 -0300

    improved Navbar component

--- frontend/src/components/Layout/Navbar/Navbar.jsx
+++ frontend/src/components/Layout/Navbar/Navbar.jsx
@@ -1,42 +1,123 @@
 import { Link, useLocation } from "react-router-dom";
-import { privateRoutes, publicRoutes } from "./navigation.js";
+import { privateRoutes, publicRoutes, userRoutes } from "./navigation.js";
 import { useAuth } from "../../../context/AuthContext";
 
 export const Navbar = () => {
   const location = useLocation();
-  const { isAuth, signout } = useAuth();
+  const { isAuth, signout, user } = useAuth();
   const isOnline = localStorage.getItem("online");
 
   return (
-    <nav className="navbar bg-base-100">
-      <ul className="p-0 menu menu-horizontal">
-        {isOnline ? (
-          <>
-            {privateRoutes.map((el) => (
-              <li
-                key={el.name}
-                className={location.pathname === el.path ? "bordered" : ""}
+    <>
+      <div className="sticky top-0 navbar bg-base-100 shadow z-[1] ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <Link to={el.path}>{el.name}</Link>
-              </li>
-            ))}
-            <li>
-              <Link onClick={signout} to="/">
-                Logout
-              </Link>
-            </li>
-          </>
-        ) : (
-          publicRoutes.map((el) => (
-            <li
-              key={el.name}
-              className={location.pathname === el.path ? "bordered" : ""}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <Link to={el.path}>{el.name}</Link>
-            </li>
-          ))
-        )}
-      </ul>
-    </nav>
+              {isOnline
+                ? privateRoutes.map((el) => (
+                    <li
+                      key={el.name}
+                      className={
+                        location.pathname === el.path ? "bordered" : ""
+                      }
+                    >
+                      <Link to={el.path}>{el.name}</Link>
+                    </li>
+                  ))
+                : publicRoutes.map((el) => (
+                    <li
+                      key={el.name}
+                      className={
+                        location.pathname === el.path ? "bordered" : ""
+                      }
+                    >
+                      <Link to={el.path}>{el.name}</Link>
+                    </li>
+                  ))}
+            </ul>
+          </div>
+          <Link to={"/"} className="text-xl btn btn-ghost">
+            DonC Alojamientos
+          </Link>
+        </div>
+        <div className="hidden navbar-center lg:flex">
+          <ul className="px-1 menu menu-horizontal">
+            {isOnline
+              ? privateRoutes.map((el) => (
+                  <li
+                    key={el.name}
+                    className={location.pathname === el.path ? "bordered" : ""}
+                  >
+                    <Link to={el.path}>{el.name}</Link>
+                  </li>
+                ))
+              : publicRoutes.map((el) => (
+                  <li
+                    key={el.name}
+                    className={location.pathname === el.path ? "bordered" : ""}
+                  >
+                    <Link to={el.path}>{el.name}</Link>
+                  </li>
+                ))}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {isAuth && (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt="Tailwind CSS Navbar component" src={user?.avatar} />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              >
+                {isAuth
+                  ? userRoutes.map((el) => (
+                      <>
+                        <li
+                          key={el.name}
+                          className={
+                            location.pathname === el.path ? "bordered" : ""
+                          }
+                        >
+                          <Link to={el.path}>{el.name}</Link>
+                        </li>
+                        <li>
+                          <button onClick={signout}>Cerrar Sesión</button>
+                        </li>
+                      </>
+                    ))
+                  : null}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
--- frontend/src/components/Layout/Navbar/navigation.js
+++ frontend/src/components/Layout/Navbar/navigation.js
@@ -2,14 +2,13 @@ export const publicRoutes = [
   { name: "Home", path: "/" },
   { name: "Login", path: "/login" },
   { name: "Register", path: "/register" },
-  { name: "Sobre mi", path: "/about" },
 ];
 
 export const privateRoutes = [
   { name: "Home", path: "/" },
-  { name: "Perfil", path: "/profile" },
   { name: "Explora", path: "/propiedades" },
   { name: "Registra tu propiedad", path: "/registro-propiedad" },
   { name: "Mis propiedades", path: "/mis-propiedades" },
-  { name: "Sobre mi", path: "/about" },
 ];
+
+export const userRoutes = [{ name: "Perfil", path: "/profile" }];
